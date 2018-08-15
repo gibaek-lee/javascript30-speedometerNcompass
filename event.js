ad6b6cae9b9246c48947f34969d3dbb1e58d619a
ginNyang2 <bbsnake10@gmail.com>
2018-08-15 17:18:37 +0900

    first commit

---
+++ event.js
@@ -0,0 +1,23 @@
+var id, options;
+
+function success(pos) {//get 'Position' object as an input
+  var crd = pos.coords;//returns a 'Coordinates' object
+  if (crd.heading && crd.speed) {//if speed is 0, heading is NaN
+    console.log('Congratulations, you get the heading and speed');
+    document.getElementById('speedRep').innerHTML=crd.speed;
+    document.getElementsByTagName('img')[0].style.transform="rotate("+crd.heading+"deg)";
+    navigator.geolocation.clearWatch(id);
+  }
+}
+
+function error(err) {
+  console.warn('ERROR(' + err.code + '): ' + err.message);
+}
+
+options = {
+  enableHighAccuracy: false,
+  timeout: 5000,
+  maximumAge: 0
+};
+
+id = navigator.geolocation.watchPosition(success, error, options);
